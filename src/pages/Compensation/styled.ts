af4b6c19f1a98d4904a0b4b236b479a6ba24c341
Nino Grigalashvili <nini.grigalashvili@gmail.com>
2022-01-15 01:03:12 +0400

    responsive for main grid table

--- src/pages/Compensation/styled.ts
+++ src/pages/Compensation/styled.ts
@@ -4,4 +4,8 @@ export const SCompensation = styled.div`
   display: grid;
   grid-template-columns: 2fr 1fr;
   gap: 100px;
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: auto;
+  }
 `;
