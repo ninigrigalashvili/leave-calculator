975c97e56af6b8528d6ae4c0a46c98881d2be2e8
Nino Grigalashvili <nini.grigalashvili@gmail.com>
2022-01-16 16:25:13 +0400

    add media queries

--- src/pages/Compensation/styled.ts
+++ src/pages/Compensation/styled.ts
@@ -2,12 +2,15 @@ import styled from "styled-components";
 
 export const SCompensation = styled.div`
   display: grid;
-  grid-template-columns: 2fr 1fr;
+  grid-template-columns: 18fr 6fr;
   gap: 100px;
-  display: flex;
-  justify-content: space-around;
 
   @media screen and (max-width: 768px) {
-    grid-template-columns: auto;
+    grid-template-columns: minmax(100px, auto);
+  }
+
+  @media screen and (min-width: 769px) and (max-width: 1441px) {
+    gap: 20px;
+    grid-template-columns: 6fr auto;
   }
 `;
