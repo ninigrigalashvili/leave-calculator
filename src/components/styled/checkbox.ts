e437203bfc44f7baf155cd6e6758e1ae540635f5
Nino Grigalashvili <nini.grigalashvili@gmail.com>
2022-01-17 14:12:46 +0400

    fix checkbox border

--- src/components/styled/checkbox.ts
+++ src/components/styled/checkbox.ts
@@ -3,22 +3,18 @@ import { Checkbox } from "antd";
 import { ThemeProps } from "utils/theme";
 
 export const SCheckbox = styled(Checkbox)`
+  .ant-checkbox-inner,
+  .ant-checkbox-inner:hover,
+  .ant-checkbox-inner:focus,
   .ant-checkbox-input:focus + .ant-checkbox-inner,
   .ant-checkbox-wrapper:hover .ant-checkbox-inner,
-  .ant-checkbox:hover .ant-checkbox-inner,
-  .ant-checkbox-inner {
-    border-image-source: ${(props) =>
-      ThemeProps[props.theme.mode]["s_checkbox_border_color"]};
-    border-image-slice: 1;
-    box-shadow: ${(props) =>
-      ThemeProps[props.theme.mode]["s_checkbox_box_shadow"]};
-  }
-  .ant-checkbox-checked .ant-checkbox-inner,
-  .ant-checkbox-indeterminate .ant-checkbox-inner {
-    background-color: transparent;
-    border-image-source: ${(props) =>
-      ThemeProps[props.theme.mode]["s_checkbox_border_color"]};
-    border-image-slice: 1;
+  .ant-checkbox:hover .ant-checkbox-inner {
+    border: double 2px transparent;
+    border-radius: 2px;
+    background-image: linear-gradient(white, white),
+      ${(props) => ThemeProps[props.theme.mode]["s_checkbox_border_color"]};
+    background-origin: border-box;
+    background-clip: content-box, border-box;
   }
 
   .ant-checkbox-checked:after {
@@ -27,9 +23,6 @@ export const SCheckbox = styled(Checkbox)`
     left: 0;
     width: 100%;
     height: 100%;
-    border-radius: 2px;
-    border-image-source: 1px solid
-      ${(props) => ThemeProps[props.theme.mode]["s_checkbox_border_color"]};
     content: "";
     -webkit-animation: antCheckboxEffect 0.36s ease-in-out;
     animation: antCheckboxEffect 0.36s ease-in-out;
@@ -42,7 +35,5 @@ export const SCheckbox = styled(Checkbox)`
   .ant-checkbox-checked:hover {
     border-color: ${(props) =>
       ThemeProps[props.theme.mode]["s_checkbox_tick_color"]} !important;
-    border-image-source: 1px solid
-      ${(props) => ThemeProps[props.theme.mode]["s_checkbox_border_color"]};
   }
 `;
