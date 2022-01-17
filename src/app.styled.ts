1e2b3423f24583d20820ea05091045b19a7679b0
Nino Grigalashvili <nini.grigalashvili@gmail.com>
2022-01-17 17:17:19 +0400

    add font family

--- src/app.styled.ts
+++ src/app.styled.ts
@@ -1,12 +1,11 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Sora:wght@400;700&display=swap');
     body {
         margin: 0;
         padding: 0;
         background: linear-gradient(90deg, #2B303A 0%, #3F4756 100%);
-        font-family: 'Sora';
+        font-family: 'Sora', sans-serif;
     }
 
 `;
